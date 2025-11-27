1af1bf4335efc892178e2101b96e31290800e4ed
01 <hosseinkhojany1380@gamil.com>
2025-11-27 23:23:23 +0330

    undo_mt5_unwanted

--- MtApi5/OrderSelectMode.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Mt5Api
-{
-    public enum OrderSelectMode
-    {
-        SELECT_BY_POS = 0,
-        SELECT_BY_TICKET = 1
-    }
-}
--- MtApi5/OrderSelectSource.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Mt5Api
-{
-    public enum OrderSelectSource
-    {
-        MODE_TRADES = 0,
-        MODE_HISTORY = 1
-    }
-}
